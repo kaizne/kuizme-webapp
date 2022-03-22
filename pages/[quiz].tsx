6254a1ef1f51df854b4c788fc59f23c75bcdfa93
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-03-22 05:00:18 -0700

    Install tailwind

---
+++ pages/[quiz].tsx
@@ -0,0 +1,22 @@
+const Quiz = () => (
+    <p>Quiz!</p>
+)
+
+export default Quiz
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { quiz: 'which-naruto-character-are-you'} }
+        ],
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    return {
+        props: {
+
+        }
+    }
+}
