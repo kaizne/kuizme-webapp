2678ae84235b1fd62545d6af3ae1ca8086749ffb
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-04-30 14:59:54 -0700

    Update Conclusion.tsx

--- components/Conclusion.tsx
+++ components/Conclusion.tsx
@@ -1,3 +1,5 @@
+
+
 const Conclusion = ({ score, total }) => (
     <div className='flex flex-col place-content-center place-items-center h-80 mt-4'>
         <div>Your score is {score} out of {total}.</div>
