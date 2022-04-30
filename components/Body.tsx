f3074afe4b47c9b6feedfc6367de742c167dc9f8
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-30 01:15:26 -0700

    Improve quiz page

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,9 +1,18 @@
 import Entry from './Entry'
+import { useEffect, useState } from 'react'
 
-const Body = ({ images, info, score, setScore, setFinish, currentQuestion, setCurrentQuestion }) => { 
+const Body = ({ images, info, score, setScore, setFinish, currentQuestion, setCurrentQuestion }) => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const newData = Object.entries(info).map(value => ({ value, sort: Math.random() }))
+                                            .sort((a, b) => a.sort - b.sort)
+                                            .map(({ value }) => value )
+        setData(newData)
+    }, [])
+     
     return (
         <div>
-            {Object.entries(info).map(([key, value], index) =>
+            {data.map(([key, value], index) =>
                 <Entry key={key} value={value} score={score} setScore={setScore} setFinish={setFinish}
                        imageUrl={findImage(String(value), images)} 
                        info={info} 
--- components/Entry.tsx
+++ components/Entry.tsx
@@ -19,34 +19,34 @@ const Entry = ({ value, imageUrl, info,
             ${currentQuestion >= question ? 'none' : 'invisible'} 
             ${currentQuestion == question ? 'animate-fadeIn' : 'none'} flex-col items-center scroll-smooth mt-8`}>
             <p className='font-semibold text-lg mb-1'>Who is this character?</p>
-            { imageUrl && <Image className='rounded-lg' src={imageUrl} width={90} height={90} /> }
+            { imageUrl && <Image className='rounded-lg' src={imageUrl} width={120} height={120} /> }
             <div className='flex flex-col w-96 justify-center items-center'>
                 <div className='grid grid-cols-2 gap-2 mt-4'>
                     <button onClick={(elem) => selectAnswer((elem.target as HTMLElement).innerHTML, value, setCorrect, 1, setButton, setDisable, 
                                                     score, setScore, question, size, setFinish, currentQuestion, setCurrentQuestion)}
                         disabled={disable}
-                        className={`w-40 h-10 font-medium border border-gray-200 
+                        className={`w-40 h-12 font-medium border border-gray-200 
                             ${button === 1 && correct === 1 ? 'bg-emerald-400' : 'none'}
                             ${button === 1 && correct === 2 ? 'bg-red-400' : 'none'}
                             ${disable === true && index === 1 ? 'bg-emerald-400' : 'none'}`}>{selection[0]}</button>
                     <button onClick={(elem) => selectAnswer((elem.target as HTMLElement).innerHTML, value, setCorrect, 2, setButton, setDisable, 
                                                     score, setScore, question, size, setFinish, currentQuestion, setCurrentQuestion)}
                         disabled={disable}
-                        className={`w-40 h-10 font-medium border border-gray-200
+                        className={`w-40 h-12 font-medium border border-gray-200
                             ${button === 2 && correct === 1 ? 'bg-emerald-400' : 'none'}
                             ${button === 2 && correct === 2 ? 'bg-red-400' : 'none'}
                             ${disable === true && index === 2 ? 'bg-emerald-400' : 'none'}`}>{selection[1]}</button>
                     <button onClick={(elem) => selectAnswer((elem.target as HTMLElement).innerHTML, value, setCorrect, 3, setButton, setDisable, 
                                                     score, setScore, question, size, setFinish, currentQuestion, setCurrentQuestion)}
                         disabled={disable}
-                        className={`w-40 h-10 font-medium border border-gray-200
+                        className={`w-40 h-12 font-medium border border-gray-200
                             ${button === 3 && correct === 1 ? 'bg-emerald-400' : 'none'}
                             ${button === 3 && correct === 2 ? 'bg-red-400' : 'none'}
                             ${disable === true && index === 3 ? 'bg-emerald-400' : 'none'}`}>{selection[2]}</button>
                     <button onClick={(elem) => selectAnswer((elem.target as HTMLElement).innerHTML, value, setCorrect, 4, setButton, setDisable, 
                                                     score, setScore, question, size, setFinish, currentQuestion, setCurrentQuestion)}
                         disabled={disable}
-                        className={`w-40 h-10 font-medium border border-gray-200
+                        className={`w-40 h-12 font-medium border border-gray-200
                             ${button === 4 && correct === 1 ? 'bg-emerald-400' : 'none'}
                             ${button === 4 && correct === 2 ? 'bg-red-400' : 'none'}
                             ${disable === true && index === 4 ? 'bg-emerald-400' : 'none'}`}>{selection[3]}</button>
@@ -76,8 +76,6 @@ const selectAnswer = (selection, value, setCorrect, button, setButton, setDisabl
                       currentQuestion, setCurrentQuestion) => {
     setDisable(true)
     setButton(button)
-    console.log(selection)
-    console.log(value)
     if (selection === value) {
         setCorrect(1)
         setScore(score+1)
@@ -85,15 +83,22 @@ const selectAnswer = (selection, value, setCorrect, button, setButton, setDisabl
         setCorrect(2)
     }
 
-    if (question + 1 === size)
+    if (question + 1 === size) {
         setFinish(true)
+        window.scrollBy({
+            top: 500,
+            left: 0,
+            behavior: 'smooth',
+        })
+    } else {
+        window.scrollBy({
+            top: 250,
+            left: 0,
+            behavior: 'smooth',
+        })
+    }
 
-    setTimeout(() => setCurrentQuestion(currentQuestion + 1), 100)
-    window.scrollBy({
-        top: 200,
-        left: 0,
-        behavior: 'smooth',
-    })
+    setTimeout(() => setCurrentQuestion(currentQuestion + 1), 100) 
 }
   
 export default Entry
