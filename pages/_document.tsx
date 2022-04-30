7e0e6d6ba2722973e163cae459f1decced3e72a6
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-29 23:59:53 -0700

    Create document page

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="..." />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap' rel='stylesheet'/>
+        <script defer data-domain="kuizme.com" src="https://plausible.io/js/plausible.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
