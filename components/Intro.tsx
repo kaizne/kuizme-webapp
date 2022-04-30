fa8fc0d94d9e4bbf8a9660bd4ca9bb481b468d6d
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-29 23:39:50 -0700

    Add featured image

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,11 @@
-const Intro = ({ title, intro, setStart }) => (
-    <div>
-        <h1 className='text-center text-xl font-bold'>{title}</h1>
-        <p className='text-center'>{intro}</p>
-        <div className='text-center mt-1'>
+import Image from 'next/image'
+
+const Intro = ({ title, intro, setStart, featured }) => (
+    <div className='flex flex-col items-center'>
+        <h1 className='text-center text-xl font-bold w-80 mb-4'>{title}</h1>
+        { featured && <Image className='rounded-lg' src={featured} width={300} height={200} /> }
+        <p className='text-center mt-4'>{intro}</p>
+        <div className='text-center mt-4'>
             <button onClick={() => setStart(true)}
             className='w-16 rounded pt-1 pb-1 bg-emerald-400 text-white font-bold'>Play</button>
         </div>
