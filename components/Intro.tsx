1516dfad12cee29f9e2948ab4bc36104f9186106
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-29 23:43:33 -0700

    Justify intro quiz text

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = ({ title, intro, setStart, featured }) => (
     <div className='flex flex-col items-center'>
         <h1 className='text-center text-xl font-bold w-80 mb-4'>{title}</h1>
         { featured && <Image className='rounded-lg' src={featured} width={300} height={200} /> }
-        <p className='text-center mt-4'>{intro}</p>
+        <p className='text-justify mt-4'>{intro}</p>
         <div className='text-center mt-4'>
             <button onClick={() => setStart(true)}
             className='w-16 rounded pt-1 pb-1 bg-emerald-400 text-white font-bold'>Play</button>
