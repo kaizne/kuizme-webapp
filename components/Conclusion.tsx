7ccf503c73deb60019a778a70d73ac6f324a3a29
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-05-01 13:07:50 -0700

    Conclusion

--- components/Conclusion.tsx
+++ components/Conclusion.tsx
@@ -1,12 +1,47 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { useLocation } from 'react-router-dom'
 
 const Conclusion = ({ type=0, score=0, total=0, character='', characterImageUrl='' }) => {
+    const percentage = Percentage(score, total)
+    var text = ''
+    if (percentage < 20) {
+        text = 'Better luck next time.'
+    }
+    else if (percentage < 40) {
+        text = 'You\'re on the right track!'
+    }
+    else if (percentage < 60) {
+        text = 'Not bad!'
+    }
+    else if (percentage < 80) {
+        text = 'Nice job!'
+    }
+    else {
+        text = 'Brilliant!'
+    }
     return (
-    <div className='fixed w-full h-full bg-opacity-95 bg-[#505050] justify-center bottom-0 left-0'>
+    <div className='fixed w-full h-full bg-opacity-95 bg-[#abadb9] justify-center bottom-0 left-0'>
         <>
         { type === 0 ? 
             <>
-            <h1 className='text-center'>Your score is {score} out of {total}.</h1>
+            <div className='flex h-screen justify-center items-center'>
+                <h1 className='text-xl text-center'>
+                You scored {score}/{total}.
+                <br></br>
+                <a className='text-2xl text-[#615477]'>{text}</a>
+                <br></br><br></br><br></br>
+                <a className='cursor-pointer text-4xl font-semibold
+                md:hover:text-[#ce3131] active:text-[#ff9c00]'
+                onClick={() => location.reload()}>Try Again</a>
+                <br></br><br></br>
+                <Link href='/'><a className='cursor-pointer text-4xl font-semibold
+                md:hover:text-[#ce3131] active:text-[#ff9c00]'>Try Other Quizzes</a></Link>
+                <br></br><br></br>
+                <Link href='/'><a className='cursor-pointer text-4xl font-semibold
+                md:hover:text-[#ce3131] active:text-[#ff9c00]'>Home</a></Link>
+                </h1>
+            </div>
             </>
             : 
             <>
@@ -17,6 +52,11 @@ const Conclusion = ({ type=0, score=0, total=0, character='', characterImageUrl=
         </>
     </div>
     )
+}
+
+function Percentage(score=0, total=0) {
+    const percentage = 100*score/total
+    return percentage
 }
 
 export default Conclusion
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,14 +10,14 @@ function Nav() {
         <nav className='flex flex-col md:flex-row ml-3 md:ml-8 mt-4 h-full scrollbar-hide'>
                 <Link href='/'>
                     <a className='w-32 md:w-44 font-Montserrate text-3xl md:text-5xl font-extrabold
-                    text-[#1BA5E4]'>KUIZME</a>
+                    text-[#1BA5E4] cursor-pointer hover:text-[#7acaef] active:text-[#acdef5]'>KUIZME</a>
                 </Link> 
                 <div className='flex flex-row md:ml-8 md:mt-4 gap-x-6 font-Poppins'>
                 {Object.entries(requests).map(([key, { title }]) => (
                 <div key={key}
-                     onClick={() => router.push(`/${key}`)}
-                     className='text-base md:text-lg font-semibold cursor-pointer transition duration-10 md:hover:scale-125 md:hover:text-[#ce3131]
-                              active:text-[#ff9c00]'>{title}</div>))}
+                    onClick={() => router.push(`/${key}`)}
+                    className='text-base md:text-lg font-semibold cursor-pointer transition duration-10 md:hover:scale-125 md:hover:text-[#ce3131]
+                    active:text-[#ff9c00]'>{title}</div>))}
                 </div>
             <div className='absolute top-0 right-0 bg-gradient-to-l
             from-[#ffffff] h-10 w-1/12'></div>
