e8fefcd2255a050f0f1ebc61458f66851da24ee3
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-01 01:42:36 -0700

    Category page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,12 +7,12 @@ import Link from 'next/link'
 function Nav() {
     const router = useRouter()
     return (
-        <nav className='flex flex-col md:flex-row ml-3 md:ml-8 mt-4 overflow-x-scroll scrollbar-hide'>
+        <nav className='flex flex-col md:flex-row ml-3 md:ml-8 mt-4 h-full scrollbar-hide'>
                 <Link href='/'>
                     <a className='w-32 md:w-44 font-Montserrate text-3xl md:text-5xl font-extrabold
                     text-[#1BA5E4]'>KUIZME</a>
                 </Link> 
-                <div className='flex flex-row md:ml-4 md:mt-3 gap-x-4'>
+                <div className='flex flex-row md:ml-8 md:mt-4 gap-x-6 font-Poppins'>
                 {Object.entries(requests).map(([key, { title }]) => (
                 <div key={key}
                      onClick={() => router.push(`/${key}`)}
--- pages/anime.tsx
+++ pages/anime.tsx
@@ -1,18 +1,32 @@
-import Link from 'next/link'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import Image from 'next/image'
 
-const Anime = () => (
+const Anime = ({ quizData }) => (
   <div>
     <Nav />
-    <p>
-      <br></br>
-      <Link href="/guess-the-demon-slayer-character">
-        <a>Guess the Demon Slayer Character Quiz</a>
-      </Link>
-    </p>
+    <div className='ml-3 md:ml-8 mt-4 font-Poppins'>
+        <h1 className='text-xl mb-2 font-semibold'>Categories</h1>
+        <div className='flex flex-col items-center w-40 h-40 border rounded-lg'>
+            <div className='mt-1'>
+                <Image className='rounded-lg' src={'/category/naruto.png'} width={120} height={120} />
+            </div>
+            <div className='text-xl -mt-1 font-bold'>Naruto</div>
+        </div>
+    </div>
     <Footer />
   </div>
 )
 
 export default Anime
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(`https://kuizme-strapi-ao8qx.ondigitalocean.app/api/quizzes?populate=*`)
+    const data = await res.json()
+    const quizData = data.data
+    return {
+        props: {
+            quizData
+        }
+    }
+}
