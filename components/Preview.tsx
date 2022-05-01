8eeb65f87634c5a4b1b9df6b2dd6849d44b613c2
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-30 20:02:28 -0700

    Home page new

---
+++ components/Preview.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Preview = ({ slug, title, thumbnail }) => {
+    return (
+        <Link href={slug}>
+            <div className='md:w-80 hover:bg-gray-300'>
+            <div className='border rounded font-Poppins h-32 flex flex-row'>
+                <div className='w-32 mt-1 ml-1'>
+                { thumbnail && <Image className='rounded-lg' src={thumbnail} width={120} height={120} /> }
+                </div>
+                <div className='w-40 mt-8 ml-4'>{title}</div>
+            </div>
+            </div>
+        </Link>
+    )
+}
+
+export default Preview
