bcf16ea67a2dc77f8a33446ceed9467c32b96f61
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-05-01 23:34:24 -0700

    Border Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,8 @@ function Footer() {
         positionText = 'relative'
     }*/}
     return (
-        <div className='bottom-0 w-full'>
-            <div className='bg-black text-white grid grid-cols-2 
+        <div className='relative bottom-0 w-full'>
+            <div className='border-t-4 border-black text-black grid grid-cols-2 
             sm:grid-cols-4 md:py-3 justify-center'>
                 <div className='text-center'>
                     <Link href='/'>
