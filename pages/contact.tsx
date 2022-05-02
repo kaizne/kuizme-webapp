5d873590c060b65f297d246eadbfffd9cd9093c8
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-01 22:59:14 -0700

    Remove fix footer

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,7 +3,7 @@ import Footer from '../components/Footer'
 
 const contact = () => (
   <>
-    <div className='flex h-screen justify-center items-center'>
+    <div className='flex min-h-screen justify-center items-center'>
       <div className='font-Poppins text-2xl'>Reach us at&nbsp;</div>
       <div className='font-Poppins text-2xl text-[#E1341E]'>contact@kuizme.com<br></br></div>
     </div>
