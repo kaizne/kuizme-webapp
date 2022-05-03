fa0f771dc09342c1e7aed5b59777b0713de2e2a9
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-05-03 01:02:24 -0700

    About, Contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,30 @@
 import Link from 'next/link'
 import Footer from '../components/Footer'
+import Image from 'next/image'
 
 const contact = () => (
   <>
-    <div className='flex min-h-screen justify-center items-center'>
-      <div className='font-Poppins text-2xl'>Reach us at&nbsp;</div>
-      <div className='font-Poppins text-2xl text-[#E1341E]'>contact@kuizme.com<br></br></div>
+    <div className='flex flex-col flex-wrap min-h-screen w-full justify-center content-center space-y-2'>
+      <div className='flex basis-full justify-center'>
+        <div className='font-Poppins text-2xl'>Reach us at&nbsp;</div>
+        <div className='font-Poppins text-2xl text-[#E1341E]'>contact@kuizme.com</div> 
+      </div>
+      <div className='flex basis-full justify-center space-x-20'>
+        <div>
+          <Link href='https://www.instagram.com/kuiz.me/'>
+            <Image src='/instagram.png' width={62} height={62} className='cursor-pointer'></Image>
+          </Link>
+        </div>
+        <div>
+          <Link href='https://twitter.com/kuiz_me'>
+            <Image src='/twitter.png' width={62} height={51} className='cursor-pointer'></Image> 
+          </Link>
+        </div>
+      </div>
+      <div className='flex basis-full justify-center space-x-20 font-Poppins'>
+        <a>Instagram</a>
+        <a>Twitter</a>
+      </div>
     </div>
     <Footer />
   </>
