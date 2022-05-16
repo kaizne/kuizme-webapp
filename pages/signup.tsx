03b0b744ed128e9176a490f22a8cfc98e55a3ed2
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-15 22:43:25 -0700

    Signup page text

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,7 +3,7 @@ const Signup = () => {
         <>
             <div className='min-h-screen'>
                 <div className='flex flex-col'>
-                    <h1 className='text-center font-Poppins text-3xl text-red-600 font-semibold py-[25%]'>Coming soon!</h1>
+                    <h1 className='text-center font-Poppins text-3xl text-red-600 font-semibold py-[25%]'>Under development.</h1>
                 </div>
             </div>
         </>
