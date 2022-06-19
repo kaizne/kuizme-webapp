99d168172eb5127bfd562aed0bed3e788b8bb187
Kaizne <github@kaizne.com>
2022-06-19 04:00:55 -0700

    Change home

--- components/Home/Section.tsx
+++ components/Home/Section.tsx
@@ -1,12 +1,12 @@
 import Preview from '../Preview'
 
-const Section = ({ title, quizData, type }) => {
+const Section = ({ title, quizData }) => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col items-center'>
             <div className='text-center text-2xl font-semibold mt-4 mb-4'>{title}</div>
             <div className='flex flex-col justify-center md:flex-row flex-wrap 
                             md:gap-x-4 gap-y-2 md:gap-y-4 ml-2 mr-2 mt-1'>
-            { quizData.slice(0,8).map((elem, index) => 
+            { quizData.map((elem, index) => 
                 <Preview key={index} 
                          slug={elem.attributes.slug}
                          title={elem.attributes.title} 
