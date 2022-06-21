f04dc6535351d5fe54da249e8b5b1ba78e6482cc
Kaizne <github@kaizne.com>
2022-06-21 00:41:34 -0700

    Category image size

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 const Category = ({category, slug, title}) => {
     return (
         <Link href={`${category}/${slug}`}>
-        <div className='flex flex-col items-center 
-                        w-40 bg-white rounded shadow 
+        <div className='w-80 h-48 flex flex-col items-center 
+                      bg-white rounded shadow 
                       hover:bg-gray-300 cursor-pointer'>
-            <div className='w-40 h-40'>
-                <Image className='rounded-t' src={`/category/${slug}.png`} width={160} height={160} />
+            <div className='relative w-80 h-48'>
+                <Image className='rounded-t' src={`/category/${slug}.png`} layout='fill' />
             </div>
             <div className='flex h-12 place-items-center'>
                 <div className='text-md font-medium text-center'>{title}</div>
