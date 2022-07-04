0becdf6886850d73a94dc4b4fc175b12a5792ed8
Kaizne <github@kaizne.com>
2022-07-03 23:32:10 -0700

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,22 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="..." />
+        <link rel='stylesheet' href='...' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-        <script defer data-domain="kuizme.com" src="https://plausible.io/js/plausible.js"></script>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Roboto:wght@400;500;700&display=swap' rel='stylesheet' />
+        <script defer data-domain='kuizme.com' src='https://plausible.io/js/plausible.js' />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
+        <script>
+          dangerouslySetInnerHTML={{
+            __html: 
+              `window.dataLayer = window.dataLayer || []
+               function gtag(){ dataLayer.push(arguments) }
+               gtag('js', new Date())
+               gtag('config', '${process.env.GOOGLE_ANALYTICS}')
+            })`,
+          }}
+        </script>
       </Head>
       <body>
         <Main />
