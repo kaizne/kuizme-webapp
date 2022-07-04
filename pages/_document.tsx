5d3dd6d66635c2df9af44e69e799ff69fdd200e4
Kaizne <github@kaizne.com>
2022-07-03 23:46:30 -0700

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,14 +11,13 @@ export default function Document() {
         <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Roboto:wght@400;500;700&display=swap' rel='stylesheet' />
         <script defer data-domain='kuizme.com' src='https://plausible.io/js/plausible.js' />
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: 
-              `window.dataLayer = window.dataLayer || []
-               function gtag(){ dataLayer.push(arguments) }
-               gtag('js', new Date())
-               gtag('config', '${process.env.GOOGLE_ANALYTICS}')
-            })`,
+        <script dangerouslySetInnerHTML={{
+          __html: 
+            `window.dataLayer = window.dataLayer || []
+              function gtag() { dataLayer.push(arguments) }
+              gtag('js', new Date())
+              gtag('config', '${process.env.GOOGLE_ANALYTICS}', { page_path: window.location.pathname })
+            `,
           }}
         />
       </Head>
