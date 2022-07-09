1d681b8f2a8c9c933dd7bb593f2a6285e37f64c6
Kaizne <github@kaizne.com>
2022-07-08 22:21:01 -0700

    demon slayer section

--- components/Home/SectionEntry.tsx
+++ components/Home/SectionEntry.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 const SectionEntry = ({ slug, title, image }) => {
     return (
         <Link href={`/${slug}`}>
-            <div className='w-60 md:w-72 cursor-pointer rounded-lg bg-white'>
+            <div className='w-56 md:w-72 cursor-pointer bg-white'>
             <div className='flex flex-col'>
-                <div className='relative w-60 h-40 md:w-72 md:h-44 hover:bg-indigo-600'>
+                <div className='relative w-56 h-32 md:w-72 md:h-44 hover:bg-indigo-600'>
                 { image && <Image src={image} layout='fill'
-                                className='rounded hover:-translate-y-1.5' /> }
+                                className='hover:-translate-y-1.5' /> }
                 </div>
-                <div className='w-60 md:w-72 mt-1 text-sm font-semibold truncate '>{title}</div>
+                <div className='w-56 md:w-72 mt-1 text-sm font-semibold truncate'>{title}</div>
             </div>
             </div>
         </Link>
