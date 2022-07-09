0b3413889355a1f69e08975bf553980c5c9e6dda
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-09 00:44:00 -0700

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -6,9 +6,9 @@ const Search = () => {
     return (
         <div className='min-h-screen'>
             <ChevronLeftIcon className='h-6 w-6 absolute mt-3 ml-6' onClick={() => history.back()}/>
-            <input type='text' placeholder='Search...' className='bg-gray-300 w-64 ml-[4.6rem] mt-2 indent-2 h-8
+            <input type='text' placeholder='Search...' className='bg-gray-200 w-64 ml-[4.6rem] mt-2 indent-2 h-8
             border-2 border-gray-400 focus:border-indigo-600 focus:outline-none focus:bg-white
-            placeholder:text-gray-500' autoFocus>
+            placeholder:text-gray-700' autoFocus>
             </input>
         </div>
     )
