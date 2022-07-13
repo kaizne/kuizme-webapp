9595d0bd187818da9b12967d70f3e852a71ee624
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-12 23:40:16 -0700

    nav bar

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -40,13 +40,20 @@ const Nav = () => {
     return (
         <>
         {(asPath !== '/signup' && asPath !== '/signin' && asPath !== '/verified') &&
-        <nav className='sticky top-0 z-50 flex flex-row h-14 w-vh
+        <nav className='sticky top-0 z-50 flex flex-row h-14 w-vh justify-between
                     bg-white shadow-lg'>
-                <div className='absolute mt-2 ml-2'>
-                <Link href='/'>
-                    <a className='text-2xl md:text-4xl font-extrabold text-indigo-600 md:hover:text-indigo-400
-                    cursor-pointer'>KUIZME</a>
-                </Link>
+                <div className='flex flex-row mt-3.5 md:mt-2 ml-[1rem] w-42 md:w-64'>
+                    <Link href='/'>
+                        <a className='text-2xl md:text-4xl font-extrabold text-indigo-600 md:hover:text-indigo-400
+                        cursor-pointer'>KUIZME</a>
+                    </Link>
+                    <div className='flex flex-row mt-[0.2rem] md:mt-[0.5rem] ml-[1rem]'>
+                    <Link href='/anime'>
+                        <a className='text-lg md:text-2xl font-bold text-black md:hover:text-indigo-600
+                        cursor-pointer'>Browse</a>
+                    </Link>
+                    </div>
+                </div>
                 { /*
                   <div className='flex flex-row gap-x-2 md:mt-3 md:gap-x-6'>
                   {Object.entries(requests).map(([key, { title }]) => (
@@ -59,15 +66,7 @@ const Nav = () => {
                   </div>
                   */ 
                 }
-                </div>
-                <div className='absolute mt-3 md:mt-4 ml-28 md:ml-48'>
-                <Link href='/anime'>
-                    <a className='text-lg md:text-2xl font-bold text-black md:hover:text-indigo-600
-                    cursor-pointer'>Browse</a>
-                </Link>
-                </div>
-                <div className='flex flex-box absolute right-1/4 mr-[0.5rem] md:right-1/3 md:mr-[9.6rem] mt-3 md:mt-4
-                gap-x-0.5'>
+                <div className='flex flex-row mt-[1.1rem] md:mt-4 gap-x-0.5'>
                     <input type='text' placeholder='Search' className='w-4 md:w-64 h-[1.7rem] bg-gray-300 border-2 border-gray-300 
                     rounded-l hover:border-gray-500 focus:bg-white indent-2 placeholder:text-gray-500 focus:border-indigo-600 
                     focus:outline-none hidden md:block'>
@@ -75,18 +74,21 @@ const Nav = () => {
                     <div className='w-[1.7rem] h-[1.7rem] bg-gray-400 border-2 border-gray-400 rounded-r hidden md:block'>
                         <SearchIcon className='w-4 h-4 mt-[0.2rem] ml-[0.2rem]'/>
                     </div>
-                    <div className='w-6 h-6 md:hidden mr-[1.4rem]' 
+                    <div className='w-6 h-6 md:hidden' 
                         onClick={() => router.push('/search')}>
                         <SearchIcon className='w-6 h-6'/>
                     </div>
                 </div>
-                { !profile ? 
-                    <Link href='/signin'>
-                        <button className='absolute w-16 h-8 mt-1.5 md:mt-3 right-0 mr-2
-                            text-sm text-white font-semibold bg-indigo-600 rounded md:hover:bg-indigo-400'>
-                            Sign In
-                        </button>
-                    </Link> :
+                { !profile ?
+                    <div className='flex flex-row mt-[0.7rem] md:mt-3 relative right-0 mr-[1rem] md:w-20 lg:w-64 justify-end'>
+                        <Link href='/signin'>
+                            <button className='w-16 h-8 text-sm text-white font-semibold bg-indigo-600 rounded 
+                            md:hover:bg-indigo-400'>
+                                Sign In
+                            </button>
+                        </Link>
+                    </div> 
+                    :
                     <div ref={ref} className='relative w-28 h-28'>
                         <button className='w-10 mt-2 ml-14 mr-4 md:mr-4 rounded md:hover:bg-gray-300'
                                 onClick={() => setDropDown(!dropDown)}>
