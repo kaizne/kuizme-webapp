0d0a7c3f15b05e0dfcf3e4e87c72d555b3c141d4
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-15 22:53:56 -0700

    changed user icon positioning and dropdown

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from 'react'
 import {
     MenuAlt1Icon,
     SearchIcon,
+    UserIcon,
 } from '@heroicons/react/outline'
 
 const Nav = () => {
@@ -53,12 +54,12 @@ const Nav = () => {
                     </div>
                     <div className='mt-[0.2rem] md:mt-[0.5rem] ml-[1rem]'>
                     <Link href='/anime'>
-                        <a className='text-lg md:text-2xl font-bold text-black md:hover:text-indigo-600
+                        <a className='text-lg md:text-2xl font-bold text-black sm:hover:text-indigo-600
                         cursor-pointer'>Browse</a>
                     </Link>
                     </div>
                     <div className='ml-[1rem] md:mt-[0.4rem]'>
-                    <MenuAlt1Icon className='h-8 w-8 hover:cursor-pointer hover:stroke-indigo-600' 
+                    <MenuAlt1Icon className='h-8 w-8 hover:cursor-pointer sm:hover:stroke-indigo-600' 
                                     onClick={() => setDrop(!drop)}/>
                     </div>
                 </div>
@@ -82,7 +83,7 @@ const Nav = () => {
                     <div className='w-[1.7rem] h-[1.7rem] bg-gray-400 border-2 border-gray-400 rounded-r hidden md:block'>
                         <SearchIcon className='w-4 h-4 mt-[0.2rem] ml-[0.2rem]'/>
                     </div>
-                    <div className='w-6 h-6 md:hidden' 
+                    <div className='w-6 h-6 md:hidden mr-[0.1rem]' 
                         onClick={() => router.push('/search')}>
                         <SearchIcon className='w-6 h-6'/>
                     </div>
@@ -97,20 +98,23 @@ const Nav = () => {
                         </Link>
                     </div> 
                     :
-                    <div ref={ref} className='relative w-28 h-28'>
-                        <button className='w-10 mt-2 ml-14 mr-4 md:mr-4 rounded md:hover:bg-gray-300'
+                    <div ref={ref} className='flex flex-row md:mt-[0.1rem] relative right-0 mr-[1rem] 
+                                            md:w-20 lg:w-64 justify-end'>
+                        <button className=''
                                 onClick={() => setDropDown(!dropDown)}>
-                            <Image src='/profile.svg' width={30} height={30} />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 sm:hover:fill-indigo-600" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                            </svg>
                         </button>
-                        <div className={`flex flex-col gap-y-1 w-24 h-16 bg-white shadow rounded
-                                        ${dropDown ? 'none' : 'hidden'}`}>
+                        <div className={`flex flex-col gap-y-1 w-32 h-20 bg-white shadow-lg rounded absolute
+                                        pl-3 py-2 right-0 sm:ml-[10rem] mt-[3rem] ${dropDown ? 'none' : 'hidden'}`}>
                             <Link href='/profile'>
-                            <button className='ml-1 mt-2 w-20 text-left text-base
-                                            md:hover:bg-gray-200'>My Profile</button>
+                            <button className='text-left text-sm w-[6rem] pl-1 py-1.5
+                                            md:hover:bg-gray-200 md:hover:rounded'>My Profile</button>
                             </Link>
                             <button onClick={() => signOut()}
-                                    className='ml-1 w-20 text-left text-base
-                                             md:hover:bg-gray-200'>Sign Out</button>
+                                    className='text-left text-sm w-[6rem] pl-1 py-1.5
+                                             md:hover:bg-gray-200 md:hover:rounded'>Sign Out</button>
                         </div>
                     </div> }
                 <div className={`flex flex-col gap-y-1 w-40 h-40 bg-white shadow-lg rounded absolute left-0
