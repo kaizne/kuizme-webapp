720943c4389242b651bc0a9d8bca2fd239b74806
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-07-18 23:46:03 -0700

    fix anime page

--- pages/anime.tsx
+++ pages/anime.tsx
@@ -3,10 +3,10 @@ import Category from '../components/Category'
 
 const Anime = ({ quizData }) => {
     return (
-        <div className='min-h-screen w-vh mt-4'>
+        <div className='min-h-screen w-screen mt-4'>
             <div className='grid justify-items-center'>
                 <h1 className='text-2xl mb-4 font-semibold'>Anime</h1>
-                <div className='grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-4'>
+                <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-4 gap-y-4'>
                 {Categories.map((elem, i) => 
                     <Category key={i} category={elem.category} image={elem.image} link={elem.link} />)}
                 </div>
