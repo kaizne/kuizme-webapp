2fc799b7d85ec3523538895b14519b63a6998ac7
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-07-21 20:12:17 -0700

    fix border and hover problem

--- components/Category.tsx
+++ components/Category.tsx
@@ -5,9 +5,10 @@ const Category = ({ category, image, link }) => {
     return (
         <Link href={`/anime/${link}`}>
             <div className='flex flex-col hover:cursor-pointer'>
-                <div className='relative w-40 h-52 flex-none rounded-md md:hover:bg-indigo-600'>
+                <div className='relative w-40 h-52 flex-none 
+                                rounded-md overflow-hidden md:hover:bg-indigo-600'>
                     <Image src={`/category/${image}.jpg`} layout='fill' 
-                           className='rounded-md md:hover:-translate-y-1.5'/>
+                           className='md:hover:-translate-y-1.5'/>
                 </div>
                 <div className='mt-1 font-semibold text-sm truncate overflow-hidden w-36'>{category}</div>
             </div>
--- components/Home/SectionEntry.tsx
+++ components/Home/SectionEntry.tsx
@@ -4,18 +4,17 @@ import Link from 'next/link'
 const SectionEntry = ({ slug, title, image }) => {
 
     return (
-        <Link href={`/${slug}`} >
-            <div className='w-56 md:w-72 cursor-pointer bg-white'>
-                <div className='flex flex-col'>
-                    <div className='relative w-56 md:w-72 h-36 md:h-44 rounded-md md:hover:bg-indigo-600 '>
+        <Link href={`/${slug}`}>
+                <div className='flex flex-col cursor-pointer'>
+                    <div className='relative w-56 md:w-72 h-36 md:h-44 
+                                    rounded-md overflow-hidden md:hover:bg-indigo-600'>
                     { image && <Image src={image} layout='fill'
-                                      className='rounded-md md:hover:-translate-y-1.5' /> }
+                                      className='md:hover:-translate-y-1.5'/> }
                     </div>
                     <div className='w-56 md:w-72 mt-1 text-sm font-semibold truncate'>
                         {title}
                     </div>
                 </div>
-            </div>
         </Link>
     )
 }
