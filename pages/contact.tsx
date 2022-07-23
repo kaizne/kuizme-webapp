010935a69c205957b3040d92f903bda980881a23
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-22 22:42:52 -0700

    contact social media links added

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const contact = () => {
   return (
   <>
@@ -29,12 +31,18 @@ const contact = () => {
     </div>
     <div className='flex flex-row h-20v sm:h-20v 
     justify-center items-start gap-x-8 md:gap-x-16'>
-      <img src='/facebook.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
-      md:w-[5rem] md:h-[5rem]'/>
-      <img src='/instagram.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
-      md:w-[5rem] md:h-[5rem]'/>
-      <img src='/twitter.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
-      md:w-[5rem] md:h-[5rem]'/>
+      <a href='https://www.facebook.com/kuizmeofficial' target='_blank'>
+        <img src='/facebook.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
+        md:w-[5rem] md:h-[5rem]'/>
+      </a>
+      <a href='https://www.instagram.com/kuiz.me/?hl=en' target='_blank'>
+        <img src='/instagram.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
+        md:w-[5rem] md:h-[5rem]'/>  
+      </a>
+      <a href='https://twitter.com/kuizmeofficial' target='_blank'>
+        <img src='/twitter.svg' className='hover:cursor-pointer w-[3.5rem] h-[3.5rem]
+        md:w-[5rem] md:h-[5rem]'/>  
+      </a>
     </div>
     <div className='flex row items-center justify-center h-7.5v sticky bg-gray-900 
      bottom-0 text-white text-center text-sm md:text-base lg:text-2xl'>
