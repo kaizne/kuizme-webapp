e7eab492cbb9ee4158e5bc285d87d19aefc0f6e5
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-30 13:25:42 -0700

    profile placeholder text

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -33,16 +33,19 @@ const Profile = () => {
     }
 
     return (
-        <div className='min-h-screen'>
-                <div className={`${profile ? 'none' : 'hidden'} mt-4`}>
-                    <h1 className='text-center text-xl'>My Profile</h1>
-                    <div className='mt-1 text-center'>{user.username}</div>
+        <div className='min-h-vh'>
+                <div className={`${profile ? 'none' : 'hidden'} mt-12 flex flex-row justify-center`}>
+                    <h1 className='text-xl text-rose-600'>New perks and features will be added regularly. Please check your email inbox
+                        for updates on the latest content.
+                    </h1>
+                    {/*<div className='mt-1 text-center'>{user.username}</div>
                     <div className='mt-1 text-center font-semibold'>Liked Quizzes</div>
                     {user.library && user.library.map((elem, index) => 
                         <div key={index} className='text-center'>{elem}</div>
                     )}
+                    */}
                 </div> 
-                <div className={`${profile ? 'hidden' : 'none'} mt-4 text-center`}>
+                <div className={`${profile ? 'hidden' : 'none'} mt-12 text-center text-xl text-rose-600`}>
                     Please sign in or sign up to access your profile.</div>
         </div>
     )
