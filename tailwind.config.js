e1260c167032133d7b34da7e8500008bc9992f9a
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-30 00:38:07 -0700

    add 3xl breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,7 +48,26 @@ module.exports = {
             "80v": "80vh",
             "90v": "90vh",
             "100v": "100vh",
-          },
+          }
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+
+        '3xl': '1792px'
+        // => @media (min-width: 1792px) { ... }
       },
     },
     plugins: [require('tailwind-scrollbar-hide')],
