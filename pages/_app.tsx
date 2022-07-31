fb3de9cb9123ffbb525dfbe975341474011b6cf9
simonbailie <84828430+simonbailie@users.noreply.github.com>
2022-07-31 01:33:49 -0700

    cookies consent on all pages + ui change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Nav from '../components/Nav/Nav'
-import Footer from '../components/Footer'
+import { useEffect, useState } from 'react'
+import { XIcon } from '@heroicons/react/outline'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const consentProperty = 'cookieConsent'
+  const [showConsent, setShowConsent] = useState(false)
+  
+  const saveToLocalStorage = () => localStorage.setItem(consentProperty, 'consent')
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          if (checkForShowConsent(consentProperty)) {
+              setShowConsent(true)
+          }
+      }, 2500);
+  }, [])
+
   return (
     <>
       <Head>
@@ -17,9 +31,22 @@ function MyApp({ Component, pageProps }: AppProps) {
         {
         //<Footer />
         }
+        <div className={`flex flex-row items-center justify-center sticky bottom-0 w-full bg-indigo-600 gap-x-2 
+        sm:gap-x-4 md:gap-x-6 lg:gap-x-8 py-3 ${showConsent ? 'none' : 'hidden'}`}>
+            <p className='text-xs md:text-sm text-white w-5/6'>We and our partners use cookies to personalize your experience
+            and for analytics purposes. By using our website and services, you agree to our use of
+            cookies as described in our <a href='/privacy' 
+            className='hover:cursor-pointer font-semibold'>Privacy Policy</a>.</p>
+            <XIcon className='h-[1.5rem] w-[1.5rem] hover:cursor-pointer stroke-white'
+            onClick={() => { saveToLocalStorage(); setShowConsent(false) }}></XIcon>
+        </div>
       </div>
     </>
   );
+}
+
+function checkForShowConsent(consentProperty) {
+  return !localStorage.getItem(consentProperty)
 }
 
 export default MyApp
--- pages/anime/[anime].tsx
+++ pages/anime/[anime].tsx
@@ -3,7 +3,7 @@ import Preview from '../../components/Preview'
 
 const Anime = ({ animeName, quizData }) => {
     return (
-        <div className='min-h-vh'>
+        <div className='min-h-screen'>
             <div className='text-lg font-bold mt-4 ml-4 md:ml-8'>{animeName}</div>
             <div className='flex flex-col md:flex-row flex-wrap md:gap-x-4 gap-y-2
                             ml-3 md:ml-8 mr-3 mt-1'>
